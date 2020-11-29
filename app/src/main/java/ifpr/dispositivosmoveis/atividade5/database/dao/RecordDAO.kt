6fef0c7fbb51f33d68ce1138348875c5592ef04a
patrickcamargo7 <patrickcsilv@gmail.com>
2020-11-29 16:52:32 -0300

    Tela de saldo concluída

--- app/src/main/java/ifpr/dispositivosmoveis/atividade5/database/dao/RecordDAO.kt
+++ app/src/main/java/ifpr/dispositivosmoveis/atividade5/database/dao/RecordDAO.kt
@@ -8,11 +8,11 @@ interface RecordDAO {
     @Query("SELECT * FROM records WHERE user_id = :userId")
     fun getAll(userId: Long): List<Record>
 
-    @Query("SELECT * FROM records WHERE user_id = :userId AND value >= 0")
-    fun getAllPayments(userId: Long): List<Record>
+    @Query("SELECT sum(value) FROM records WHERE user_id = :userId AND value < 0")
+    fun getTotalPayments(userId: Long): Float
 
-    @Query("SELECT * FROM records WHERE user_id = :userId AND value < 0")
-    fun getAllReceives(userId: Long): List<Record>
+    @Query("SELECT sum(value) FROM records WHERE user_id = :userId AND value >= 0")
+    fun getTotalReceives(userId: Long): Float
 
     @Query("SELECT * FROM records WHERE id IN (:ids)")
     fun getAllByIds(ids: IntArray): List<Record>
